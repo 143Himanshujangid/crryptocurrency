1d6b415b9fc1ae5b3ee50e2fb970ede2812dc923
143Himanshujangid <146875926+143Himanshujangid@users.noreply.github.com>
2024-11-24 10:43:06 +0530

    Update app.py

--- st2/app.py
+++ st2/app.py
@@ -45,7 +45,7 @@ def add_custom_css():
         <style>
         /* Admin Login Page Styling */
         .login-page {
-            border: 3px solid read;
+            border: 2px solid read;
             border-radius: 15px;
             padding: 30px;
             background-color: #f9f9f9;
@@ -54,7 +54,7 @@ def add_custom_css():
 
         /* Styling Sidebar Items */
         .stSelectbox, .stRadio, .stMetric {
-            border: 3px solid red;
+            border: 1.5px solid red;
             border-radius: 15px;
             padding: 5px;
         }
